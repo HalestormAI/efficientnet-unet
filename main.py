5800735a4f7524a9ea54e07593bb6d8352555452
Ian Hales <contact@ian-hales.com>
2021-12-19 21:36:16 +0000

    Put the model and inputs onto the GPU

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ from utils import parse_args
 if __name__ == "__main__":
     args = parse_args()
 
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
     train_dataloader = DataLoader(dataset.train(args), batch_size=args.batch_size, shuffle=True)
     val_dataloader = DataLoader(dataset.eval(args), batch_size=args.batch_size, shuffle=True)
 
@@ -20,6 +22,7 @@ if __name__ == "__main__":
 
     model = EffUnet(args.model_size, num_classes=num_classes, activate_logits=False, remove_bn=True)
     summary(model, input_size=(args.batch_size, 3, args.image_size, args.image_size))
+    model.to(device)
 
     optimizer = torch.optim.Adam(model.parameters(), 0.01)
     criterion = nn.CrossEntropyLoss()
@@ -28,13 +31,17 @@ if __name__ == "__main__":
     for epoch in range(args.epochs):
         model.train()
         for batch_idx, (inputs, targets) in enumerate(tqdm.tqdm(train_dataloader)):
+            inputs = inputs.to(device)
+            targets = targets.to(device)
+
             optimizer.zero_grad()
             logits = model(inputs)
 
             tgt_one_hot = F.one_hot(targets, num_classes).permute(0, 3, 1, 2).float()
             cross_entropy_loss = criterion(logits, targets)
+            print("Cross entropy loss is on GPU? ", cross_entropy_loss.is_cuda)
             dice = dice_loss(logits, tgt_one_hot, multiclass=True)
-            total_loss = cross_entropy_loss
+            total_loss = dice + cross_entropy_loss
 
             total_loss.backward()
             optimizer.step()
