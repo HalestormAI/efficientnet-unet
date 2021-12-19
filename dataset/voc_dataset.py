e02b63811b29bef58e7c688600ac6430c0000784
Ian Hales <contact@ian-hales.com>
2021-12-19 21:35:21 +0000

    Stop the eval dataset re-extracting once the training set is extracted.

--- dataset/voc_dataset.py
+++ dataset/voc_dataset.py
@@ -12,6 +12,5 @@ def get_dataset(dataset_name, transform):
 
     train_dataset = VOCSegmentation(f"data/voc_{ds_year}", image_set="train", year=ds_year, transforms=transform,
                                     download=download)
-    eval_dataset = VOCSegmentation(f"data/voc_{ds_year}", image_set="val", year=ds_year, transforms=transform,
-                                   download=download)
+    eval_dataset = VOCSegmentation(f"data/voc_{ds_year}", image_set="val", year=ds_year, transforms=transform)
     return train_dataset, eval_dataset
